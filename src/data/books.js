831a39e8b608d102aee1c04938fedd2391e112cd
Luis Fernando Nieves Nieves <this.luisnieves@gmail.com>
2023-07-06 19:48:01 -0600

    Creation of the components which make up the structure of the book and the list of books, added id, title, img, opinion and author

---
+++ src/data/books.js
@@ -0,0 +1,30 @@
+export const books = [
+	{
+		id: 'clean-code',
+		title: 'Clean Code: Manual de desarrollo agil de software',
+		img: 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1436202607i/3735293.jpg',
+		opinion: 'Es un libro que hay que leer tendiendo en cuenta perspectiva, no hay que tomarse todo como si fuera ley de programacion, hay muchas cosas interesantes que aun son vigentes en 2023, pero no es parra nada recomendable para personas que estan aprendiendo, todos los ejemplos estan escritos en codigo Java y POO',
+		author: 'Robert C.Martin'
+	},
+	{
+		id: 'eloquent-javascript',
+		title: 'Eloquent Javascript',
+		img: 'https://eloquentjavascript.net/img/cover.jpg',
+		opinion: '',
+		author: 'Marijn Haverbeke'
+	},
+	{
+		id: 'solid-code',
+		title: 'Solid Code - Best Practices',
+		img: 'https://m.media-amazon.com/images/I/51Y2-4-x1-L._SY466_.jpg',
+		opinion: '',
+		author: 'Donis Marshall'
+	},
+	{
+		id: 'react-up',
+		title: 'React: Up & Running',
+		img: 'https://m.media-amazon.com/images/I/81h9Ilh0nIL._SY466_.jpg',
+		opinion: '',
+		author: 'Stoyan Stefanov'
+	}
+]
